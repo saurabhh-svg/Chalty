6491dfac04479d8a06fe826ce7ce2987d5f4a85b
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-07-21 12:07:25 +0530

    Setting directories

---
+++ public/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
